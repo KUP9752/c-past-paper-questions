71b906cc91a0c9f7cc43cc7f0533ae7089d3d86e
kup21 <kup21@ic.ac.uk>
2022-06-11 10:47:19 +0100

    kup21 : Done the doublets paper

--- doublets/doublets.c
+++ doublets/doublets.c
@@ -1,21 +1,96 @@
 #include "doublets.h"
+#include<assert.h>
 
 /* ------------------ YOUR CODE HERE ------------------- */
 
 bool valid_step(dictionary_t *dict, const char *curr_word, const char *next_word) {
-  return false;
+  //check rule 2:
+  bool oneChange = false;
+  //Same length?
+  if (strlen(curr_word) != strlen(next_word)) return false;
+
+  // only 1 letter difference.
+  for (int i = 0; curr_word[i]; i++) {
+    if (curr_word[i] != next_word[i]) {
+      if (oneChange) {
+        return false;
+      } else {
+          oneChange = true;
+        }
+    }
+  }
+
+  return find(dict, next_word) && oneChange;
 }
 
+void printOut(const char *word, bool upCond) {
+  for (int i = 0; word[i]; i++) {
+    printf("%c", upCond ? toupper(word[i]) : tolower(word[i]));
+  }
+  printf("\n");
+}
 void print_chain(const char **chain) {
+  assert(chain);
+  printOut(chain[0], true);
+  int i = 1;
+  for(; chain[i + 1]; i++) {
+    printOut(chain[i], false);
+  }
+  //i is now the final word, before the NULL end
+  printOut(chain[i], true);
   return;
 }
 
 bool valid_chain(dictionary_t *dict, const char **chain) {
+
+  for (int i = 0; chain[i] && chain[i+1]; i++) {
+    if (!valid_step(dict, chain[i], chain[i+1])) {
+      return false;
+    }
+    //check if everything before i, equals i
+    for (int j = 0; j < i; j++) { 
+      if (!strcmp(chain[i], chain[j])){
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+void chainadd(const char *str, int place, const char **chain) {
+  chain[place] = strcpy(malloc(strlen(str) + 1), str);
+}
+
+bool used(const char **chain, char* word) {
+  for (char** w = (char**)chain; *w; w++) {
+    if (!strcmp(*w, word)) return true;
+  }
   return false;
 }
 
-bool find_chain(dictionary_t *dict, const char *start_word, 
-                const char *target_word, const char **chain, int max_words) {
-  return false;
+bool find_chain_aux(dictionary_t *dict, const char *start_word, const char *target_word, const char **chain, int max_words, int word_no) {
+  if (word_no == max_words) return false;
+  else if (valid_step(dict, start_word, target_word)) {
+    chainadd(target_word, word_no, chain);
+    return true;
+  } else {
+    for (int c = 0; start_word[c]; c++) {
+      for (char i = 'A'; i <= 'Z'; i++) {
+        char newword[MAX_WORD_SIZE];
+        strcpy(newword, start_word)[c] = i;
+        if (!used(chain, newword) && i != start_word[c] && find(dict, newword)) {
+          chainadd(newword, word_no, chain);
+          if (find_chain_aux(dict, newword, target_word, chain, max_words, word_no + 1)) return true;
+        }
+      }
+    }
+    return false;
+  }
+}
+
+bool find_chain(dictionary_t *dict, const char *start_word, const char *target_word, const char **chain, int max_words) {
+  chainadd(start_word, 0, chain);
+  return find_chain_aux(dict, start_word, target_word, chain, max_words, 1);
 }
 
--- doublets/trie.c
+++ doublets/trie.c
@@ -1,4 +1,5 @@
 #include "trie.h"
+#define ALPHABET_SIZE 26
 
 dictionary_t *create_dict(void) {
   return create_node();
@@ -11,23 +12,78 @@ void free_dict(dictionary_t *root) {
 /* ------------ YOUR CODE BELOW -------------- */
 
 bool find(dictionary_t *root, const char *word) {
-  return false;
+  dictionary_t *currentNode = root;
+  int c;
+  if (!root) {
+    return false;
+  }
+
+  for (int i = 0; word[i]; i++) {
+
+    if (word[i] < 'A' || word[i] > 'Z') {
+      return false;
+    }
+
+    c = word[i] - 'A';
+    currentNode = currentNode -> children[c];
+
+    if (!currentNode) { //child doesn't exist
+      return false;
+    }
+  }
+  return currentNode -> end_of_word;
 }
 
 bool insert(dictionary_t *root, const char *word) {
-  return false;
+  dictionary_t *currentNode = root;
+  int c;
+  for (int i = 0; word[i]; i++) {
+    if (word[i] < 'A' || word[i] > 'Z') {
+      return false;
+    }
+    c = word[i] - 'A';
+
+    if (!currentNode -> children[c]) {
+      currentNode -> children[c] = create_node();
+    }
+    currentNode = currentNode -> children[c];
+  }
+  // we exit when currentNode is the last letter
+  currentNode -> end_of_word = true;
+  return true;
 }
 
 dictionary_t *create_node(void) {
-  return NULL;
+  dictionary_t *newNode = (dictionary_t *) malloc(sizeof(dictionary_t));
+  newNode -> end_of_word = false;
+  newNode -> children = calloc(ALPHABET_SIZE, sizeof(dictionary_t *));
+  return newNode;
 }
 
 void free_node(dictionary_t *root) {
-  return;
+  if (root) {
+    for(int i = 0; i < ALPHABET_SIZE; i++) {
+      free_node(root -> children[i]);
+    }
+    free(root -> children);
+  free(root);
+  }
 }
 
 bool load_from_file(dictionary_t *root, const char *filename) {
-  return false;
+  FILE *filePtr = fopen(filename, "r");
+  if (!filePtr) {
+    perror("File could not be opened");
+    return false;
+  }
+
+  char buffer[MAX_WORD_SIZE];
+  while (fgets(buffer, MAX_WORD_SIZE, filePtr)){
+    insert(root, strtok(buffer, "\n"));    
+  }
+  fclose(filePtr);
+  return true;
+
 }
 
 
